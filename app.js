2d80a0daac6a58387b3e99cce4e5785ef2e53c60
Sadir Irfan <93542008+Sadir25@users.noreply.github.com>
2023-08-30 22:56:18 +0530

    done

--- app.js
+++ app.js
@@ -8,9 +8,6 @@
 // alert(username)
 
 // document is an api present in every browser
-var btnTranslate = document.querySelector("#btn-translate")
-var txtInput=document.querySelector("#text-input");
-var outputDiv = document.querySelector("#output")
 
 // outputDiv.innerText = "Sadir Irfan"
 // console.log(outputDiv)
@@ -22,10 +19,35 @@ var outputDiv = document.querySelector("#output")
 
 //addEventListener(what event, what it should do after the event happens)
 
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput=document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output")
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?"+"text="+text
+}
+
 btnTranslate.addEventListener("click",clickEventHandler)
 
+function errorHandler(error){
+    console.log("Error occured",error)
+    alert("Something wrong with server, try aagin after sometime")
+}
+
 function clickEventHandler(){
-    outputDiv.innerText = "klsjakljajslkj"+" "+txtInput.value
+    var inputText= txtInput.value; //input
+    
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText =translatedText //output
+    })
+    .catch(errorHandler)
 }
 
 
