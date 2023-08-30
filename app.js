da7ecfdd02f24784d751f77a5da8b7b037630366
Sadir Irfan <93542008+Sadir25@users.noreply.github.com>
2023-08-30 15:15:10 +0530

    Taking input and console

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // prompt is used to take input
-let username=prompt("Give me your username")
+
+// let username=prompt("Give me your username")
 
 
 // alert-- is used to pop output on the browser (but it is not the best way to shoe output)
-alert(username)
+
+// alert(username)
+
+// document is an api present in every browser
+var btnTranslate = document.querySelector("#btn-translate")
+// console.log(btnTranslate)
+
+
+var txtInput=document.querySelector("#text-input")
+console.log(txtInput)
+//how do users interact with on app--- using eventListner
+
+//addEventListener(what event, what it should do after the event happens)
+
+btnTranslate.addEventListener("click",clickEventHandler)
+
+function clickEventHandler(){
+    console.log("clicked")
+    console.log("input",txtInput.value)
+}
 
 
