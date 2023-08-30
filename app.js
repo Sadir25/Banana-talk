85a23742bb9d1826059484b7d27d4e4a5d5b06d3
Sadir Irfan <93542008+Sadir25@users.noreply.github.com>
2023-08-30 14:36:07 +0530

    minor change in js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-// console.log("Hello working from different file")
-// alert("Hey! Sadir Complete the work")
+// prompt is used to take input
 let username=prompt("Give me your username")
+
+
+// alert-- is used to pop output on the browser (but it is not the best way to shoe output)
 alert(username)
+
+
